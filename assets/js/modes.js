0b3bf13e8879a8d15aef332634aedb1c2da7084e
guillermoandrae <guillermoandraefisher@gmail.com>
2022-01-01 12:33:56 -0500

    Updated toggle controls to add an actual toggle.

--- assets/js/modes.js
+++ assets/js/modes.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
-    $("#dark-mode-toggle").click(function () {
+    const currentMode = $("#theme-mode").attr("href");
+    const isDarkModeOn = (currentMode.indexOf("normal") == -1);
+    let $darkModeToggle = $("#dark-mode-toggle");
+    let $darkModeToggleIcon = $darkModeToggle.find("i");
+
+    if (isDarkModeOn) {
+        $darkModeToggleIcon.removeClass("fa-toggle-off");
+        $darkModeToggleIcon.addClass("fa-toggle-on");
+    } else {
+        $darkModeToggleIcon.removeClass("fa-toggle-on");
+        $darkModeToggleIcon.addClass("fa-toggle-off");
+    }
+
+    $darkModeToggle.click(function () {
         let newUrl = "/?mode=";
         let themeMode = "normal";
-        const currentMode = $("#theme-mode").attr("href");
-        themeMode = (currentMode.indexOf("normal") == -1) ? "normal" : "dark";
+        themeMode = (isDarkModeOn) ? "normal" : "dark";
         localStorage.setItem("theme", themeMode);
         window.location.href = newUrl + themeMode;
     });
